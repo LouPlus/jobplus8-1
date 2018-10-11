ba4fb7a48a4a7784d4a6c4b73125ab976898cf76
949410591 <949410591@qq.com>
2018-10-11 22:30:00 +0800

    fix #16 fix #17  flask_app config.py

--- jobplus8-1/jobplus/app.py
+++ jobplus8-1/jobplus/app.py
@@ -4,4 +4,9 @@ from .config import configs
 def create_app(config):
 	app = Flask(__name__)
 	app.config.from_object(configs.get(config))
+
+	@app.route('/test')
+	def test():
+		return 'This is test!!!!!!!!!!!!'
+
 	return app
