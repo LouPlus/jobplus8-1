d808e6b737f0829460ef5478e1332dbf94fbdf06
949410591 <949410591@qq.com>
2018-10-11 22:30:00 +0800

    fix #16 fix #17  flask_app config.py

--- jobplus8-1/jobplus/config.py
+++ jobplus8-1/jobplus/config.py
@@ -7,6 +7,7 @@ class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class ProductionConfig(BaseConfig):
     pass
 
