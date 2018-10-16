2df610b909059b2e326e615778ea4cd99144fffb
mythomasliu <274966585@qq.com>
2018-10-16 22:47:30 +0800

    用户资料修改

--- jobplus8-1/jobplus/models.py
+++ jobplus8-1/jobplus/models.py
@@ -31,7 +31,7 @@ class User(Base,UserMixin):
     email = db.Column(db.String(64),unique=True,index=True,nullable=False)
     role = db.Column(db.SmallInteger,default=ROLE_EMPLOYEE)
     _password = db.Column('password',db.String(256),nullable=False)
-    phonenumber = db.Column(db.Integer)
+    phonenumber = db.Column(db.Text)
     work_experience = db.Column(db.SmallInteger)
     jobs = db.relationship('Job',secondary=user_job)
     upload_resume_url =db.Column(db.String(64))
