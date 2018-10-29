acab6d4f6209e06691b60d29ad0aed8b599fc071
949410591 <949410591@qq.com>
2018-10-30 02:42:49 +0800

    1

--- jobplus8-1/migrations/versions/55551f164231_v1_0.py
+++ jobplus8-1/migrations/versions/55551f164231_v1_0.py
@@ -1,8 +1,8 @@
 """v1.0
 
-Revision ID: 9a56e5189478
+Revision ID: 55551f164231
 Revises: 
-Create Date: 2018-10-29 19:20:53.795289
+Create Date: 2018-10-30 02:34:18.982619
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9a56e5189478'
+revision = '55551f164231'
 down_revision = None
 branch_labels = None
 depends_on = None
